4bf0ce6d377a3783353ef02181c47ab7e16a4229
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-06-04 13:36:04 -0400

    tests for horizontal and vertical wins pass

--- tests/test_tic_tac_toe_winner.py
+++ tests/test_tic_tac_toe_winner.py
@@ -1,6 +1,6 @@
 # from tic
 # from tic_tac_toe_winner import tic_tac_toe_winner
-from tic_tac_toe_winner import InvalidMatrix, tic_tac_toe_winner
+from tic_tac_toe_winner import InvalidMatrix, GameNotOver, tic_tac_toe_winner
 import pytest
 
 # @pytest.mark.skip(reason="Not possible to test at this time")
@@ -39,6 +39,71 @@ def test_invalid_rel_num_Xs_Os_raises_error():
     with pytest.raises(InvalidMatrix):
         tic_tac_toe_winner(matrix_more_X)
 
+# @pytest.mark.skip(reason="Not possible to test at this time")
+def test_horizontal_win():
+    # arrange
+    matrix_row_1_win = [
+        ['X', 'X', 'X'],
+        ['O', 'O', ''],
+        ['O', 'X', 'O']
+        ]
+    matrix_row_2_win = [
+        ['X', 'O', 'X'],
+        ['O', 'O', 'O'],
+        ['X', '', 'X']
+    ]
+    matrix_row_3_win = [
+        ['O','X','X'],
+        ['X','','X'],
+        ['O','O','O']
+    ]
+    # act
+    row_1_win = tic_tac_toe_winner(matrix_row_1_win)
+    row_2_win = tic_tac_toe_winner(matrix_row_2_win)
+    row_3_win = tic_tac_toe_winner(matrix_row_3_win)
+
+    # assert
+    assert row_1_win == 'X'
+    assert row_2_win == 'O'
+    assert row_3_win == 'O'
+
+# @pytest.mark.skip(reason="Not possible to test at this time")
+def test_vertical_win():
+    # arrange
+    matrix_column_0_win = [
+        ['X', 'O', 'X'],
+        ['X', 'O', ''],
+        ['X', '', 'O']
+        ]
+    matrix_column_1_win = [
+        ['X', 'O', 'X'],
+        ['O', 'O', ''],
+        ['X', 'O', 'X']
+    ]
+    matrix_column_2_win = [
+        ['O','','X'],
+        ['X','','X'],
+        ['O','O','X']
+    ]
+    # act
+    column_0_win = tic_tac_toe_winner(matrix_column_0_win)
+    column_1_win = tic_tac_toe_winner(matrix_column_1_win)
+    column_2_win = tic_tac_toe_winner(matrix_column_2_win)
+
+    # assert
+    assert column_0_win == 'X'
+    assert column_1_win == 'O'
+    assert column_2_win == 'X'
+
+# @pytest.mark.skip(reason="Not possible to test at this time")
+def test_diagonal_win():
+    # arrange
+
+@pytest.mark.skip(reason="Not possible to test at this time")
+def test_game_not_over():
+    # arrange
+    pass
+
 @pytest.mark.skip(reason="Not possible to test at this time")
 def test_x_winner_board_not_full():
     # arrange
@@ -52,7 +117,7 @@ def test_x_winner_board_not_full():
     # assert
     assert result == 'X'
     
-        
+    
 
 
 
--- tic_tac_toe_winner.py
+++ tic_tac_toe_winner.py
@@ -3,20 +3,29 @@ import pytest
 class InvalidMatrix(Exception):
     pass
 
+class GameNotOver(Exception):
+    pass
+
 def tic_tac_toe_winner(matrix):
     # Check for valid input
-    if not is_valid_input(matrix):
+    is_valid = is_valid_input(matrix)
+    if not is_valid:
         raise InvalidMatrix
-    # Check for game still in progress
     # Check for horizontal win
-        # For row in matrix:
-            # if all 'X' or all 'O' return row[0]
+    # For row in matrix:
+    for row in matrix:
+        # if all 'X' or all 'O' return row[0]
+        if row[0] == row[1] == row[2]:
+            return row[0]
     # Check for vertical win
-        # For column in matrix (i in range len(matrix[0]))
-            # if all 'X' or all 'O' return matrix[0][column]
+    # For column in matrix (i in range len(matrix[0]))
+    for i in range(len(matrix[0])):
+        # if all 'X' or all 'O' return matrix[0][column]
+        if matrix[0][i] == matrix[1][i] == matrix[2][i]:
+            return matrix[0][i]
     # Check for diagonal win
-        # if matrix[0][0] == [matrix][1][1] == [matrix][2][2] or matrix[0][2] == matrix[1][1] == matrix[2][0] 
-            # return matrix[1][1]
+    if matrix[0][0] == [matrix][1][1] == [matrix][2][2] or matrix[0][2] == matrix[1][1] == matrix[2][0]:
+        return matrix[1][1]
     # Check if more than two blank spaces in matrix
         # initialize num_blank_spaces variable
         # for row in matrix
@@ -31,22 +40,25 @@ def is_valid_input(matrix):
     # Check all elements are 'X', 'O', or ''
     num_X = 0
     num_O = 0
+    num_space = 0
     for row in matrix:
-        for column in matrix:
+        for column in row:
             if column not in ['X','O','']:
                 return False
             elif column == 'X':
                 num_X += 1
-            else:
+            elif column == 'O':
                 num_O += 1
+            else:
+                num_space += 1
             
     # Check number of 'X' between number of 'O' +/- 1
     if num_X < num_O - 1 or num_X > num_O + 1 or num_O < num_X - 1 or num_O > num_X + 1:
         return False
     # Check matrix is 3 x 3
-    # Return True if valid, raise InvalidMatrix error otherwise
-    return False
-
-def is_game_over(matrix):
-    
-    pass
+    if len(matrix) != 3:
+        return False
+    if len(matrix[0]) != 3 or len(matrix[1]) != 3 or len(matrix[2]) != 3:
+        return False
+    # Return True and num blank spaces if valid
+    return (True, num_space)
